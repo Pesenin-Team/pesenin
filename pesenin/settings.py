50e0302c2890accf8c74b789e722085cc2cfb750
mhuda1 <mushaffahuda01@gmail.com>
2019-10-14 17:09:46 +0700

    adding login app

--- pesenin/settings.py
+++ pesenin/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'comingsoon.apps.ComingsoonConfig',
+    'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

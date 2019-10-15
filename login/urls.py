c817e92b73b4b0bb35723977f28ea6812977790c
unknown <mushaffahuda01@gmail.com>
2019-10-15 16:39:02 +0700

    adding front-end

--- login/urls.py
+++ login/urls.py
@@ -5,4 +5,7 @@ app_name = 'login'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    # path('login/', cas_views.LoginView.as_view(), name='login'),
+    # path('logout/', cas_views.LogoutView.as_view(), name='logout'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
 def home(request):
+    '''Render Home Pesenin'''
     return render(request, 'home/index.html')
+
+
+@login_required(login_url='sso_ui:login')
+def profile(request):
+    """Render profile page."""
+    context = {
+        "profile": {
+            "Nama lengkap": request.user.get_full_name(),
+            "Nama pengguna": request.user.username,
+            "Alamat surel": request.user.email,
+            "Kode organisasi": request.user.profile.org_code,
+            "Peran pengguna": request.user.profile.role,
+            "Nomor Pokok Mahasiswa": request.user.profile.npm,
+            "Fakultas": request.user.profile.faculty,
+            "Jurusan": request.user.profile.study_program,
+            "Program pendidikan": request.user.profile.educational_program
+        }
+    }
+    return render(request, "home/profile.html", context)

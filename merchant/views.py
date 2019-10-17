8a74deac47d8948a986d68c845a6b0bb07bdbb64
Kukuh Hafiyyan <kukuh89@gmail.com>
2019-10-17 18:24:23 +0700

    photo

--- merchant/views.py
+++ merchant/views.py
@@ -5,8 +5,9 @@ from .models import Merchant
 
 
 def merchant(request):
-    
+    nama_merchant = "Warkop DKI"
+    decs = "Sedia Mie Instant, Jeroan, dan Kopi"
+    link_gambar = "../../static/merchant/img/"
     return render(request, 'merchant/index.html')
 
-def makanan(request):
-    return render(request, 'makanan/index.html')
+

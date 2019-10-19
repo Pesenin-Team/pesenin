9b30e6559b9d35ed38173db51d042fec5df54021
mhuda1 <mushaffahuda01@gmail.com>
2019-10-19 20:34:55 +0700

    adding search form queue

--- login/views.py
+++ login/views.py
@@ -22,12 +22,12 @@ def profile(request):
             "Nama lengkap": request.user.get_full_name(),
             "Nama pengguna": request.user.username,
             "Alamat surel": request.user.email,
-            "Kode organisasi": request.user.profile.org_code,
-            "Peran pengguna": request.user.profile.role,
+            # "Kode organisasi": request.user.profile.org_code,
+            # "Peran pengguna": request.user.profile.role,
             "Nomor Pokok Mahasiswa": request.user.profile.npm,
             "Fakultas": request.user.profile.faculty,
             "Jurusan": request.user.profile.study_program,
-            "Program pendidikan": request.user.profile.educational_program
+            # "Program pendidikan": request.user.profile.educational_program
         }
     }
     return render(request, "home/profile.html", context)

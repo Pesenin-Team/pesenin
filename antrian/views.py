bbb1d14bcaaf1aa0f7eb6f96f7ee42caf74caf8a
mhuda1 <mushaffahuda01@gmail.com>
2019-10-19 18:53:35 +0700

    konekin semua

--- antrian/views.py
+++ antrian/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Queue
+from merchant.models import Makanan
 # Create your views here.
 
 
 def antri(request):
     if (request.user.is_authenticated):
         user = request.user
-        # makanan = "Ayam mentega"
-        # info = "Not Ready"
-        # link = "../../../static/queue/img/ayam-mentega.jpg"
-        # antrian_baru = Queue(
-        #     user=user, nama_makanan=makanan, status=info, foto=link)
-        # antrian_baru.save()
         queue_list = Queue.objects.filter(user=user)
         content = {
             "antrian": queue_list
         }
         return render(request, 'queue/index.html', content)
     return render(request, 'queue/index.html',)
+
+
+def tambah_antrian(request, pk):
+    if (request.user.is_authenticated):
+        user = request.user
+        makanan = Makanan.objects.get(pk=pk)
+        nama = makanan.nama
+        info = makanan.deskripsi
+        link = makanan.foto
+        antrian_baru = Queue(
+            user=user, nama_makanan=nama, status=info, foto=link)
+        antrian_baru.save()
+        return redirect('antrian:queue')
